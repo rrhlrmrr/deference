05207123f38124a2a7aa2907040c7e119d24e8f7
rrhlrmrr <rrhlrmrr@naver.com>
2025-02-07 17:33:51 +0900

    update about & add panel speak

--- components/About/about.js
+++ components/About/about.js
@@ -12,10 +12,10 @@ function About() {
         layout="fill"
         className="bg-03"
       />
-      <div className="pt-[160px] lg:py-[100px] container flex items-center justify-center w-full">
+      <div className="pt-[80px] lg:py-[100px] container flex items-center justify-center w-full">
         <div className="w-[1120px] lg:w-full flex lg:flex-col-reverse items-center justify-between">
           <div
-            className="w-[500px] h-[500px] lg:w-[350px] lg:h-[350px] lg:mt-10" // border, p-10, borderRadius 스타일 제거
+            className="w-[500px] h-[500px] lg:w-[350px] lg:h-[350px] lg:mt-10"
             style={{}}
           >
             <Image
@@ -24,7 +24,7 @@ function About() {
               layout="intrinsic"
               width={1000}
               height={1000}
-              objectFit="contain" // 이미지 비율 유지
+              objectFit="contain"
             />
           </div>
           <div className="w-[600px] ml-10 lg:ml-0 lg:w-full lg:text-center">
@@ -33,17 +33,29 @@ function About() {
               DE-FERENCE 2025
             </Heading>
             <Paragraph typeStyle="md" className="lg:mt-10">
-              DE-Ference 2025: Towards a Seamless Future
+              DE-FERENCE는 서울대학교 블록체인 학회, 디사이퍼(Dechiper)가
+              2018년부터 매년 주관하는 컨퍼런스로, 디사이퍼 학회원들과 블록체인
+              업계의 빌더들이 한자리에 모여 업계에 대해 함께 논의하고
+              네트워킹하는 행사입니다.
               <br />
-              서울대 블록체인 학회에서 주최하는 제 8회 디퍼런스에 여러분을
-              초대합니다. <br />
-              블록체인과 AI의 융합이 열어가는 새로운 시대, 디사이퍼와 함께
-              알아가는 시간을 가져보세요! <br /> 이번 DE-FERENCE에서는
-              <strong>
-                &ldquo;Towards a Seamless Future of Blockchain&rdquo;
-              </strong>
-              이라는 주제 아래, 학회원들은 블록체인의 최신 기술 트렌드, AI와의
-              접목, 그리고 실용적인 연구 성과를 공유하며 미래를 조망합니다.
+              <br /> 2025년 2월 15일 드림플러스 강남에서 열리는 제 8회
+              디퍼런스는
+              <strong>"Towards a Seamless Future of Blockchain"</strong>이라는
+              주제로 진행됩니다. 본 컨퍼런스에서는 블록체인의 최신 기술 트렌드,
+              AI와의 접목, 그리고 실용적인 연구 성과를 공유하며 블록체인의
+              미래를 조망할 예정입니다.
+              <br />
+              <br /> 디사이퍼는 학문적 연구를 넘어, 블록체인 생태계의 발전을
+              위해 학계과 산업을 연결하는 플랫폼 역할을 수행하고 있습니다. 이번
+              컨퍼런스에서는 디사이퍼 학회원들이 한 학기 동안 연구하고 개발한
+              블록체인 프로젝트를 발표하며, 업계 전문가들과 함께 심도 있는
+              논의와 인사이트를 교류하는 장이 될 것입니다.
+              <br />
+              <br /> 2025년, 블록체인은 AI 기술과의 융합을 통해 혁신적인 도약을
+              이루고 있습니다. 기술의 경계를 허물며 더욱 효율적인 미래로
+              나아가는 이 여정에 디사이퍼가 함께합니다. DE-FERENCE 2025에서
+              변화하는 블록체인 산업의 흐름을 경험하고, 미래를 만들어갈 여러분을
+              초대합니다.
             </Paragraph>
           </div>
         </div>
